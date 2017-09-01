33b14960bf4cd093a47d4ae73e509dde7c3f0efe
Marko Jokic <mjokic@users.noreply.github.com>
2017-09-01 12:30:28 +0200

    Trying to add tolbar on preference activity. No luck yet!

--- app/src/main/java/com/wifi/ethereumtracker/activities/PreferencesActivity.java
+++ app/src/main/java/com/wifi/ethereumtracker/activities/PreferencesActivity.java
@@ -1,24 +1,37 @@
 package com.wifi.ethereumtracker.activities;
 
 
+import android.app.Activity;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
+import com.wifi.ethereumtracker.R;
 import com.wifi.ethereumtracker.fragments.PreferencesFragment;
 
-public class PreferencesActivity extends PreferenceActivity{
+public class PreferencesActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_preferences);
+
         PreferencesFragment preferencesFragment = new PreferencesFragment();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.removeAllViews(); // removing refresh button imageView
+        toolbar.setTitle("Settings");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(android.R.id.content, preferencesFragment)
+                .replace(R.id.preferencesFragment, preferencesFragment)
                 .commit();
     }
 
