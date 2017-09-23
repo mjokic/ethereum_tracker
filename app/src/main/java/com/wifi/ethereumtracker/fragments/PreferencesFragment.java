346c4b3cc9c2f8ea0571b2ff07246041a298dff5
Marko Jokic <mjokic@users.noreply.github.com>
2017-09-23 15:57:27 +0200

    Made code more beautiful in PreferencesFragment class

--- app/src/main/java/com/wifi/ethereumtracker/fragments/PreferencesFragment.java
+++ app/src/main/java/com/wifi/ethereumtracker/fragments/PreferencesFragment.java
@@ -25,11 +25,13 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-public class PreferencesFragment extends PreferenceFragment {
+public class PreferencesFragment extends PreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
-    public ListPreference listPreferenceCurrencySettings;
-    public EditTextPreference minNotifyValueEditTextPref;
-    public EditTextPreference maxNotifyValueEditTextPref;
+    private ListPreference listPreferenceCurrencySettings;
+    private EditTextPreference minNotifyValueEditTextPref;
+    private EditTextPreference maxNotifyValueEditTextPref;
+    private ListPreference listPreferenceCheckInterval;
 
 
     @Override
@@ -41,154 +43,144 @@ public class PreferencesFragment extends PreferenceFragment {
         List<Profile> profiles = dbHelper.getProfiles();
 
 
-        final ListPreference listPreferenceSourceSettings = (ListPreference) findPreference("sourceSettings");
+        ListPreference listPreferenceSourceSettings = (ListPreference) findPreference("sourceSettings");
+
         listPreferenceCurrencySettings = (ListPreference) findPreference("currencySettings");
 
 
         // checking if myValue is valid
         EditTextPreference myValueEditTextPref = (EditTextPreference) findPreference("myValue");
-        myValueEditTextPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object o) {
-                try {
-                    Double.parseDouble((String) o);
-                }catch (Exception ex){
-                    return false;
-                }
-
-                return true;
-            }
-        });
+        myValueEditTextPref.setOnPreferenceChangeListener(this);
 
 
         SwitchPreference switchPreferenceEnableNotifications = (SwitchPreference)
                 findPreference("enableNotificationsSettings");
+        switchPreferenceEnableNotifications.setOnPreferenceChangeListener(this);
 
-        final ListPreference listPreferenceCheckInterval = (ListPreference) findPreference("checkInterval");
+
+        listPreferenceCheckInterval = (ListPreference) findPreference("checkInterval");
+        listPreferenceCheckInterval.setOnPreferenceChangeListener(this);
+
 
         minNotifyValueEditTextPref = (EditTextPreference) findPreference("valueMinNotify");
+        minNotifyValueEditTextPref.setOnPreferenceChangeListener(this);
+
         maxNotifyValueEditTextPref = (EditTextPreference) findPreference("valueMaxNotify");
-
-        minNotifyValueEditTextPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object o) {
-                try{
-                    Double.parseDouble((String) o);
-                }catch (Exception ex){
-                    return false;
-                }
-
-                return true;
-            }
-        });
-
-        maxNotifyValueEditTextPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object o) {
-                try{
-                    Double.parseDouble((String) o);
-                }catch (Exception ex){
-                    return false;
-                }
-
-                return true;
-            }
-        });
+        maxNotifyValueEditTextPref.setOnPreferenceChangeListener(this);
 
 
         // load profiles list from database and send them under as parameter
         setListPrefSourceEntries(listPreferenceSourceSettings, profiles);
-
-
-        listPreferenceSourceSettings.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object o) {
-                String s = (String) o;
-                System.out.println(s + "<-- CHOSEN!");
-
-                Profile profile = new Gson().fromJson(s, Profile.class);
-
-                setListPrefCurrencyEntries(listPreferenceCurrencySettings, profile.getCurrencies());
-
-                return true;
-            }
-        });
-
-        listPreferenceCurrencySettings.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-
-                String hey = preference.getSharedPreferences().getString("currencySettings", null);
-                System.out.println(hey + "<-- DOES IT WORK?!");
-
-                return true;
-            }
-        });
-
-        switchPreferenceEnableNotifications.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object o) {
-
-                int currentPrice = getPreferenceManager()
-                        .getSharedPreferences().getInt("currentPrice", 0);
-                int tenPercent = calculate10Percent(currentPrice);
-
-                // set default values
-                minNotifyValueEditTextPref.setText(String.valueOf(currentPrice - tenPercent));
-                maxNotifyValueEditTextPref.setText(String.valueOf(currentPrice + tenPercent));
-
-                boolean status = (boolean) o;
-
-                Intent intent = new Intent(getActivity(), AlarmReceiver.class);
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(getActivity(), 0,
-                        intent, 0);
-
-                AlarmManager alarmManager = (AlarmManager)
-                        getActivity().getSystemService(Context.ALARM_SERVICE);
-
-                int waitInterval = Integer.parseInt(listPreferenceCheckInterval.getValue());
-
-                if(status){
-                    alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
-                            Calendar.getInstance().getTimeInMillis()+waitInterval,
-                            waitInterval, pendingIntent);
-
-                }else{
-                    alarmManager.cancel(pendingIntent);
-                }
-
-                return true;
-            }
-        });
-
-
-
-        listPreferenceCheckInterval.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-            @Override
-            public boolean onPreferenceChange(Preference preference, Object o) {
-                // when interval changes cancel alarm manger and start new one,
-                // so new interval gets applied
-
-                int waitInterval = Integer.parseInt((String) o);
-
-                Intent intent = new Intent(getActivity(), AlarmReceiver.class);
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(getActivity(), 0,
-                        intent, 0);
-
-                AlarmManager alarmManager = (AlarmManager)
-                        getActivity().getSystemService(Context.ALARM_SERVICE);
-
-                alarmManager.cancel(pendingIntent);
-
-                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
-                        Calendar.getInstance().getTimeInMillis()+waitInterval,
-                        waitInterval, pendingIntent);
-
-                return true;
-            }
-        });
+        listPreferenceSourceSettings.setOnPreferenceChangeListener(this);
 
     }
+
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object o) {
+
+        switch (preference.getKey()){
+            case "sourceSettings":
+                return sourceSettingsOnChange(o);
+            case "myValue":
+                return myValueSettingsOnChange(o);
+            case "enableNotificationsSettings":
+                return enableNotfOnChange(o);
+            case "checkInterval":
+                return intervalSettOnChange(o);
+            case "valueMinNotify":
+            case "valueMaxNotify":
+                return minmaxValueOnChange(o);
+
+        }
+
+        return true;
+    }
+
+
+    // onPreferenceChange methods
+    private boolean sourceSettingsOnChange(Object object){
+        String s = (String) object;
+        Profile profile = new Gson().fromJson(s, Profile.class);
+        setListPrefCurrencyEntries(listPreferenceCurrencySettings, profile.getCurrencies());
+
+        return true;
+    }
+
+    private boolean myValueSettingsOnChange(Object object){
+        try {
+            Double.parseDouble((String) object);
+        }catch (Exception ex){
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean enableNotfOnChange(Object object){
+        int currentPrice = getPreferenceManager()
+                .getSharedPreferences().getInt("currentPrice", 0);
+        int tenPercent = calculate10Percent(currentPrice);
+
+        // set default values
+        minNotifyValueEditTextPref.setText(String.valueOf(currentPrice - tenPercent));
+        maxNotifyValueEditTextPref.setText(String.valueOf(currentPrice + tenPercent));
+
+        boolean status = (boolean) object;
+
+        Intent intent = new Intent(getActivity(), AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getActivity(), 0,
+                intent, 0);
+
+        AlarmManager alarmManager = (AlarmManager)
+                getActivity().getSystemService(Context.ALARM_SERVICE);
+
+        int waitInterval = Integer.parseInt(listPreferenceCheckInterval.getValue());
+
+        if(status){
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
+                    Calendar.getInstance().getTimeInMillis()+waitInterval,
+                    waitInterval, pendingIntent);
+
+        }else{
+            alarmManager.cancel(pendingIntent);
+        }
+
+        return true;
+    }
+
+    private boolean intervalSettOnChange(Object object){
+        // when interval changes cancel alarm manger and start new one,
+        // so new interval gets applied
+
+        int waitInterval = Integer.parseInt((String) object);
+
+        Intent intent = new Intent(getActivity(), AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getActivity(), 0,
+                intent, 0);
+
+        AlarmManager alarmManager = (AlarmManager)
+                getActivity().getSystemService(Context.ALARM_SERVICE);
+
+        alarmManager.cancel(pendingIntent);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
+                Calendar.getInstance().getTimeInMillis()+waitInterval,
+                waitInterval, pendingIntent);
+
+        return true;
+    }
+
+    private boolean minmaxValueOnChange(Object object){
+        try{
+            Double.parseDouble((String) object);
+        }catch (Exception ex){
+            return false;
+        }
+
+        return true;
+    }
+
 
 
     private void setListPrefSourceEntries(ListPreference lp, List<Profile> profiles){
@@ -216,8 +208,6 @@ public class PreferencesFragment extends PreferenceFragment {
 
     }
 
-
-
     private void setListPrefCurrencyEntries(ListPreference lp, List<String> values){
         CharSequence[] cs = values.toArray(new CharSequence[values.size()]);
         lp.setEntries(cs);
@@ -228,9 +218,8 @@ public class PreferencesFragment extends PreferenceFragment {
         }
     }
 
-
-
     private int calculate10Percent(int value){
         return value * 10 / 100;
     }
+
 }
