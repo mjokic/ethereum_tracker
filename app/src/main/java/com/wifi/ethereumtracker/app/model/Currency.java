d47053ae7fa294590f33048af3303a36d6e2b5ce
Marko Jokic <mjokic@users.noreply.github.com>
2017-11-25 12:36:19 +0100

    Added currency to the model

---
+++ app/src/main/java/com/wifi/ethereumtracker/app/model/Currency.java
@@ -0,0 +1,28 @@
+package com.wifi.ethereumtracker.app.model;
+
+public class Currency {
+
+    private String name;
+    private String sign;
+
+    public Currency(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+}
